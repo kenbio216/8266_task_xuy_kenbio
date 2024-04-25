c36007f79a4c1ebaf03f6ad12165882fcad9f057
kenbio216 <3498592827@qq.com>
2024-04-25 09:37:09 +0800

    添加了arduino.json的"output": "./build"

---
+++ build/core/core_esp8266_eboot_command.cpp.d
@@ -0,0 +1,8 @@
+e:\win_code_git\8266_task_xuy_kenbio\build\core\core_esp8266_eboot_command.cpp.o: \
+ C:\Users\xuyang\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\3.1.2\cores\esp8266\core_esp8266_eboot_command.cpp \
+ C:\Users\xuyang\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\3.1.2\cores\esp8266\CommonHFile.h \
+ e:\win_code_git\8266_task_xuy_kenbio\build\core\mqtt_esp8266_myserver.ino.globals.h \
+ C:\Users\xuyang\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\3.1.2\cores\esp8266\coredecls.h \
+ C:\Users\xuyang\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\3.1.2\cores\esp8266\core_esp8266_features.h \
+ C:\Users\xuyang\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\3.1.2\cores\esp8266/cont.h \
+ C:\Users\xuyang\AppData\Local\Arduino15\packages\esp8266\hardware\esp8266\3.1.2\cores\esp8266\eboot_command.h
